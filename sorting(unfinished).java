9b9a3808eef011b37c7f3d0eac8e5ebecc12ec25
Andreynikonov23 <76613379+Andreynikonov23@users.noreply.github.com>
2022-03-05 11:17:32 +0300

    Update sorting(unfinished).java

--- sorting(unfinished).java
+++ sorting(unfinished).java
@@ -21,40 +21,59 @@ public class Main{
             arrayList.add(reader.readLine());
         }
         System.out.println(arrayList);
+        ArrayList<String> temp = new ArrayList<>();
         int[] ints = new int[arrayList.size()];
         String[] strArr = new String[arrayList.size()];
+        int[] nulls = new int[arrayList.size()];
         for (int i = 0; i < arrayList.size(); i++) {
             try{
                 ints[i] = Integer.parseInt(String.valueOf(arrayList.get(i)));
+                if (ints[i] != 0) {
+                    temp.add("i");
+                }
             }
             catch (Exception e){
                 strArr[i] = String.valueOf(arrayList.get(i));
+                temp.add("s");
+            }
+        }
+        for (int i = 0; i < arrayList.size(); i++) {
+            if (arrayList.get(i).equals("0")){
+                nulls[i] = 1;
             }
         }
         ArrayList<Integer> integers = new ArrayList<>();
         ArrayList<String> strings = new ArrayList<>();
         for (int i = 0; i < ints.length; i++) {
-            if (ints[i] != 0){
-                integers.add(ints[i]);
-            }
+            integers.add(ints[i]);
         }
         for (int i = 0; i < strArr.length; i++) {
             if (strArr[i] != null){
                 strings.add(strArr[i]);
             }
         }
-        Collections.sort(integers);
-        Collections.sort(strings);
-        System.out.println(integers);
-        System.out.println(strings);
-        HashMap<Integer, String> map = new HashMap<>();
-        HashMap<Integer, String> map1 = new HashMap<>();
-        int sIndex = 0;
-        int iIndex = 0;
         for (int i = 0; i < arrayList.size(); i++) {
-            for (int j = 0; j < strings.size(); j++) {
-
+            if (arrayList.get(i).equals("0")){
+                integers.add(0);
             }
         }
+        Collections.sort(integers);
+        Collections.sort(strings);
+        Collections.reverse(integers);
+        System.out.println(integers);
+        System.out.println(strings);
+        ArrayList<String> newList = new ArrayList<>();
+        System.out.println(temp);
+        for (int i = 0, str=0, integer=0; i < temp.size(); i++) {
+            if (temp.get(i).equals("s")){
+                newList.add(strings.get(str));
+                str++;
+            }
+            if (temp.get(i).equals("i")){
+                newList.add(String.valueOf(integers.get(integer)));
+                integer++;
+            }
+        }
+        System.out.println(newList);
     }
 }
